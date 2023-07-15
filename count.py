6c4e22c2015574c03ce7d81206d27cc3ab041a0c
Abhishek <abhishekm.21.beis@acharya.ac.in>
2023-07-15 09:29:03 +0530

    Words counting

---
+++ count.py
@@ -0,0 +1,15 @@
+alpha, uppercase, lowercase, digit, words = 0, 0, 0, 0, 0
+str = input("Enter a Sentence: ")
+
+for i in str:
+    if i.isupper():
+        uppercase = uppercase + 1
+    elif i.islower():
+        lowercase = lowercase + 1
+    elif i.isdigit():
+        digit = digit + 1
+        
+print("Uppercase letters: ", uppercase)
+print("Lowercase letters: ", lowercase)
+print("Total digits: ", digit)
+print("Total words: ", len(str.split()))
