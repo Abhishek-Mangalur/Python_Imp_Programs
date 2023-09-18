6bccac5a37d153f936954a3461e75b4ba1c6fbe5
Mr. Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2023-09-18 17:47:50 +0530

    String Similarity

--- 3b.py
+++ 3b.py
@@ -3,8 +3,8 @@ def string_similarity(str1, str2):
     result = difflib.SequenceMatcher(a = str1.lower(), b = str2.lower())
     return result.ratio()
 
-str1 = input("Enter the String1: ")
-str2 = input("Enter the String2: ")
+str1 = input("Enter the 1st String: ")
+str2 = input("Enter the 2nd String: ")
 print("Original Strings:")
 print(str1)
 print(str2)
